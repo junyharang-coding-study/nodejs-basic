d06b2f78fa3871878289f7684ef6c15353708d2e
junyharang <junyharang8592@gmail.com>
2022-04-05 20:19:47 +0900

    First Commit

---
+++ syntax/main.js
@@ -0,0 +1,22 @@
+var http = require('http');
+var fs = require('fs');
+var app = http.createServer(function(request, response) {
+
+    var url = request.url;
+
+    if(request.url == '/') {
+        url = 'index.html';
+    }   // if(request.url == '/') 끝
+
+    if(request.url == '/favicon.ico') {
+
+        return response.writeHead(404);
+
+    }   // if(request.url == '/favicon.ico') 끝
+
+    response.writeHead(200);
+    response.end(fs.readFileSync(__dirname + url));
+
+});
+
+app.listen(3000);
